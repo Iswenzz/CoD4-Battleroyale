7f5618f95d337d92c966b6e3ec277676f2a983d5
Iswenzz <alexisnardiello@gmail.com>
2023-04-04 20:52:27 +0200

    Scripts: IWD changes

--- scripts/build_iwds.sh
+++ scripts/build_iwds.sh
@@ -7,15 +7,13 @@ rm -v _images.iwd
 rm -v _sounds.iwd
 rm -v _weapons.iwd
 rm -v _z.iwd
-rm -v _zz.iwd
 
 # SR
 cd "$SR/assets"
 7z a -r -tzip "$MOD/_images.iwd" images/*
 7z a -r -tzip "$MOD/_sounds.iwd" sound/*
 7z a -r -tzip "$MOD/_weapons.iwd" weapons/*
-7z a -r -tzip "$MOD/_z.iwd" misc/iwds/_z/*
-7z rn "$MOD/_z.iwd" misc/iwds/_z sound
+7z a -r -tzip "$MOD/_z.iwd" ./misc/iwds/_z/*
 
 # IWDs
 cd "$MOD/assets"
